60ff63e0cc431b56b263d7ba9d6cae5c73a85cd6
joao baltazar <joao-p-baltazar@telecom.pt>
2021-11-01 19:07:07 +0000

    (next) - init project
    Signed-off-by: joaopeedro <joao.p.baltazar@gmail.com>

---
+++ parque/build/precache-manifest.a8999e1ffd2172143c481403850b85da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e55115a5c91457cd901d80bf1a8899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1da58d30c13f1e933feb",
+    "url": "/static/css/main.20f91e54.chunk.css"
+  },
+  {
+    "revision": "9a91784a58bb81ca9eaa",
+    "url": "/static/js/2.f0a9c407.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f0a9c407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da58d30c13f1e933feb",
+    "url": "/static/js/main.8f7ab278.chunk.js"
+  },
+  {
+    "revision": "3c0ebffa8d3cf1a4814c",
+    "url": "/static/js/runtime-main.7757cbc1.js"
+  }
+]);
