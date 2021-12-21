7da2c62e2013cf85a6b3cebcbdb03a747ad4c52c
joaopeedro <joao.p.baltazar@gmail.com>
2021-12-21 21:26:25 +0000

    (next) - init project
    Signed-off-by: joaopeedro <joao.p.baltazar@gmail.com>

---
+++ utils/patterns/frequency.js
@@ -0,0 +1,37 @@
+//frequency counter
+
+/**
+ * give two strings, write a function to determine if the seconde string is an anagram of the first.
+ * An anagram is a wword, phrase, or name formed by rearranging the letters of another,
+ * such as cineme, formed from iceman.
+ */
+
+const validAnagramm = (firstString, secondString) => {
+  console.log(firstString.length, secondString.length);
+  if (firstString.length !== secondString.length) {
+    console.log("já foste");
+    return false;
+  }
+
+  let lookup = {};
+
+  for (const letter of firstString) {
+    // if lette existes, increment, otherwise set to 1
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  console.log(lookup);
+
+  for (const letter of secondString) {
+    if (!lookup[letter]) {
+      console.log("já foste");
+      return false;
+    }
+
+    lookup[letter] -= 1;
+  }
+  console.log(lookup);
+  return true;
+};
+
+validAnagramm("TEST", "TSET");
---
+++ utils/patterns/pointers.js
@@ -0,0 +1,18 @@
+/**
+ * Write a function wich takes in a string and return conts of each character in the string
+ */
+
+const charCount = (str) => {
+  // make obj to return at end
+  let objToReturn = {};
+
+  // loop over string for each character
+  // if the char is a number/letter AND is a key in obj, add one to count
+  // if the char is a number/letter AND not in obj, add it and set value to 1
+  // if a char is something else (space, period, etc) don't do anything.
+
+  // return obj at end
+  return objToReturn;
+};
+
+charCount("aabbcc");
